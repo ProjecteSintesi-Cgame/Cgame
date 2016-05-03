f9c7a35bad3475455eaa076a60aeab8bbe7855b7
unknown <39176.joan23@fje.edu>
2016-05-03 11:12:20 +0200

    Primera version bd

--- bd/bd_cgames.sql
+++ bd/bd_cgames.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-05-2016 a las 10:32:34
+-- Tiempo de generación: 03-05-2016 a las 11:10:22
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.6.3
 
@@ -30,8 +30,8 @@ USE `bd_cgames`;
 
 CREATE TABLE IF NOT EXISTS `tbl_chat` (
 `id_chat` int(11) NOT NULL,
-  `id_usuarioP` int(11) NOT NULL,
-  `id_usuarioC` int(11) NOT NULL
+  `usu_emailP` varchar(50) COLLATE utf8_bin NOT NULL,
+  `usu_emailC` varchar(50) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -66,8 +66,8 @@ INSERT INTO `tbl_genero` (`id_genero`, `gen_nombre`) VALUES
 
 CREATE TABLE IF NOT EXISTS `tbl_interesado` (
 `id_interes` int(11) NOT NULL,
-  `id_usuarioP` int(11) NOT NULL,
-  `id_usuarioC` int(11) NOT NULL,
+  `usu_emailP` varchar(50) COLLATE utf8_bin NOT NULL,
+  `usu_emailC` varchar(50) COLLATE utf8_bin NOT NULL,
   `id_juegos` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -82,19 +82,8 @@ CREATE TABLE IF NOT EXISTS `tbl_juego` (
   `jue_nombre` varchar(20) COLLATE utf8_bin NOT NULL,
   `id_genero` int(11) NOT NULL,
   `id_plataforma` int(11) NOT NULL,
-  `id_usuarioP` int(11) NOT NULL
+  `usu_emailP` varchar(50) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Volcado de datos para la tabla `tbl_juego`
---
-
-INSERT INTO `tbl_juego` (`id_juegos`, `jue_nombre`, `id_genero`, `id_plataforma`, `id_usuarioP`) VALUES
-(1, 'DriveClub 4', 4, 12, 1),
-(2, 'FIFA 16', 5, 12, 2),
-(3, 'World Of Warcraft', 7, 11, 2),
-(4, 'Overwatch Origins 2', 3, 11, 3),
-(5, 'Assassin´s Creed Rog', 2, 13, 1);
 
 -- --------------------------------------------------------
 
@@ -105,7 +94,8 @@ INSERT INTO `tbl_juego` (`id_juegos`, `jue_nombre`, `id_genero`, `id_plataforma`
 CREATE TABLE IF NOT EXISTS `tbl_mensajes` (
 `id_mensaje` int(11) NOT NULL,
   `mens_mensaje` text COLLATE utf8_bin NOT NULL,
-  `id_chat` int(11) NOT NULL
+  `id_chat` int(11) NOT NULL,
+  `usu_email` varchar(50) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -139,7 +129,7 @@ INSERT INTO `tbl_plataforma` (`id_plataforma`, `pla_nombre`) VALUES
 CREATE TABLE IF NOT EXISTS `tbl_usuario` (
 `id_usuario` int(11) NOT NULL,
   `usu_nombre` varchar(20) COLLATE utf8_bin NOT NULL,
-  `usu_email` varchar(35) COLLATE utf8_bin NOT NULL,
+  `usu_email` varchar(50) COLLATE utf8_bin NOT NULL,
   `usu_apellido` varchar(35) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -160,18 +150,10 @@ INSERT INTO `tbl_usuario` (`id_usuario`, `usu_nombre`, `usu_email`, `usu_apellid
 
 CREATE TABLE IF NOT EXISTS `tbl_valoracio` (
 `id_valoracion` int(11) NOT NULL,
-  `id_usuarioP` int(11) NOT NULL,
-  `id_usuarioC` int(11) NOT NULL,
+  `usu_emailP` varchar(50) COLLATE utf8_bin NOT NULL,
+  `usu_emailC` varchar(50) COLLATE utf8_bin NOT NULL,
   `val_valoracion` text COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Volcado de datos para la tabla `tbl_valoracio`
---
-
-INSERT INTO `tbl_valoracio` (`id_valoracion`, `id_usuarioP`, `id_usuarioC`, `val_valoracion`) VALUES
-(1, 1, 2, 'Muy buen vendedor y buena compra.'),
-(2, 3, 3, 'Comprador muy fiable');
 
 --
 -- Índices para tablas volcadas
@@ -181,7 +163,7 @@ INSERT INTO `tbl_valoracio` (`id_valoracion`, `id_usuarioP`, `id_usuarioC`, `val
 -- Indices de la tabla `tbl_chat`
 --
 ALTER TABLE `tbl_chat`
- ADD PRIMARY KEY (`id_chat`), ADD KEY `id_usuarioP` (`id_usuarioP`), ADD KEY `id_usuarioC` (`id_usuarioC`);
+ ADD PRIMARY KEY (`id_chat`), ADD KEY `id_usuarioP` (`usu_emailP`), ADD KEY `id_usuarioC` (`usu_emailC`);
 
 --
 -- Indices de la tabla `tbl_genero`
@@ -193,19 +175,19 @@ ALTER TABLE `tbl_genero`
 -- Indices de la tabla `tbl_interesado`
 --
 ALTER TABLE `tbl_interesado`
- ADD PRIMARY KEY (`id_interes`), ADD KEY `id_usuarioP` (`id_usuarioP`), ADD KEY `id_usuarioC` (`id_usuarioC`), ADD KEY `id_juego` (`id_juegos`);
+ ADD PRIMARY KEY (`id_interes`), ADD KEY `id_usuarioP` (`usu_emailP`), ADD KEY `id_usuarioC` (`usu_emailC`), ADD KEY `id_juego` (`id_juegos`);
 
 --
 -- Indices de la tabla `tbl_juego`
 --
 ALTER TABLE `tbl_juego`
- ADD PRIMARY KEY (`id_juegos`), ADD KEY `id_genero` (`id_genero`), ADD KEY `id_plataforma` (`id_plataforma`), ADD KEY `id_usuarioP` (`id_usuarioP`), ADD KEY `id_juegos` (`id_juegos`);
+ ADD PRIMARY KEY (`id_juegos`), ADD KEY `id_genero` (`id_genero`), ADD KEY `id_plataforma` (`id_plataforma`), ADD KEY `id_usuarioP` (`usu_emailP`), ADD KEY `id_juegos` (`id_juegos`);
 
 --
 -- Indices de la tabla `tbl_mensajes`
 --
 ALTER TABLE `tbl_mensajes`
- ADD PRIMARY KEY (`id_mensaje`), ADD KEY `id_chat` (`id_chat`);
+ ADD PRIMARY KEY (`id_mensaje`), ADD KEY `id_chat` (`id_chat`), ADD KEY `usu_email` (`usu_email`);
 
 --
 -- Indices de la tabla `tbl_plataforma`
@@ -217,13 +199,13 @@ ALTER TABLE `tbl_plataforma`
 -- Indices de la tabla `tbl_usuario`
 --
 ALTER TABLE `tbl_usuario`
- ADD PRIMARY KEY (`id_usuario`), ADD KEY `id_usuario` (`id_usuario`);
+ ADD PRIMARY KEY (`usu_email`), ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `tbl_valoracio`
 --
 ALTER TABLE `tbl_valoracio`
- ADD PRIMARY KEY (`id_valoracion`), ADD KEY `id_usuarioP` (`id_usuarioP`), ADD KEY `id_usuarioC` (`id_usuarioC`);
+ ADD PRIMARY KEY (`id_valoracion`), ADD KEY `id_usuarioP` (`usu_emailP`), ADD KEY `id_usuarioC` (`usu_emailC`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -277,37 +259,38 @@ MODIFY `id_valoracion` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 -- Filtros para la tabla `tbl_chat`
 --
 ALTER TABLE `tbl_chat`
-ADD CONSTRAINT `tbl_chat_ibfk_1` FOREIGN KEY (`id_usuarioP`) REFERENCES `tbl_usuario` (`id_usuario`),
-ADD CONSTRAINT `tbl_chat_ibfk_2` FOREIGN KEY (`id_usuarioC`) REFERENCES `tbl_usuario` (`id_usuario`);
+ADD CONSTRAINT `tbl_chat_ibfk_1` FOREIGN KEY (`usu_emailP`) REFERENCES `tbl_usuario` (`usu_email`),
+ADD CONSTRAINT `tbl_chat_ibfk_2` FOREIGN KEY (`usu_emailC`) REFERENCES `tbl_usuario` (`usu_email`);
 
 --
 -- Filtros para la tabla `tbl_interesado`
 --
 ALTER TABLE `tbl_interesado`
-ADD CONSTRAINT `tbl_interesado_ibfk_1` FOREIGN KEY (`id_usuarioP`) REFERENCES `tbl_usuario` (`id_usuario`),
-ADD CONSTRAINT `tbl_interesado_ibfk_2` FOREIGN KEY (`id_usuarioC`) REFERENCES `tbl_usuario` (`id_usuario`),
-ADD CONSTRAINT `tbl_interesado_ibfk_3` FOREIGN KEY (`id_juegos`) REFERENCES `tbl_juego` (`id_juegos`);
+ADD CONSTRAINT `tbl_interesado_ibfk_3` FOREIGN KEY (`id_juegos`) REFERENCES `tbl_juego` (`id_juegos`),
+ADD CONSTRAINT `tbl_interesado_ibfk_4` FOREIGN KEY (`usu_emailP`) REFERENCES `tbl_usuario` (`usu_email`),
+ADD CONSTRAINT `tbl_interesado_ibfk_5` FOREIGN KEY (`usu_emailC`) REFERENCES `tbl_usuario` (`usu_email`);
 
 --
 -- Filtros para la tabla `tbl_juego`
 --
 ALTER TABLE `tbl_juego`
-ADD CONSTRAINT `tbl_juego_ibfk_2` FOREIGN KEY (`id_usuarioP`) REFERENCES `tbl_usuario` (`id_usuario`),
 ADD CONSTRAINT `tbl_juego_ibfk_3` FOREIGN KEY (`id_plataforma`) REFERENCES `tbl_plataforma` (`id_plataforma`),
-ADD CONSTRAINT `tbl_juego_ibfk_4` FOREIGN KEY (`id_genero`) REFERENCES `tbl_genero` (`id_genero`);
+ADD CONSTRAINT `tbl_juego_ibfk_4` FOREIGN KEY (`id_genero`) REFERENCES `tbl_genero` (`id_genero`),
+ADD CONSTRAINT `tbl_juego_ibfk_5` FOREIGN KEY (`usu_emailP`) REFERENCES `tbl_usuario` (`usu_email`);
 
 --
 -- Filtros para la tabla `tbl_mensajes`
 --
 ALTER TABLE `tbl_mensajes`
-ADD CONSTRAINT `tbl_mensajes_ibfk_1` FOREIGN KEY (`id_chat`) REFERENCES `tbl_chat` (`id_chat`);
+ADD CONSTRAINT `tbl_mensajes_ibfk_1` FOREIGN KEY (`id_chat`) REFERENCES `tbl_chat` (`id_chat`),
+ADD CONSTRAINT `tbl_mensajes_ibfk_2` FOREIGN KEY (`usu_email`) REFERENCES `tbl_usuario` (`usu_email`);
 
 --
 -- Filtros para la tabla `tbl_valoracio`
 --
 ALTER TABLE `tbl_valoracio`
-ADD CONSTRAINT `tbl_valoracio_ibfk_1` FOREIGN KEY (`id_usuarioP`) REFERENCES `tbl_usuario` (`id_usuario`),
-ADD CONSTRAINT `tbl_valoracio_ibfk_2` FOREIGN KEY (`id_usuarioC`) REFERENCES `tbl_usuario` (`id_usuario`);
+ADD CONSTRAINT `tbl_valoracio_ibfk_1` FOREIGN KEY (`usu_emailP`) REFERENCES `tbl_usuario` (`usu_email`),
+ADD CONSTRAINT `tbl_valoracio_ibfk_2` FOREIGN KEY (`usu_emailC`) REFERENCES `tbl_usuario` (`usu_email`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
